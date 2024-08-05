344934ef6f54ea0e6ac51ff3d37ecc9d1f7f7a44
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-06 00:58:30 +0500

    feat: application

--- pyasync/applications.py
+++ pyasync/applications.py
@@ -1,1 +1,25 @@
-    
+from pyasync.routing import Router
+from pyasync.responses import Response
+
+
+class Pyasync:
+    def __init__(self):
+        self.router = Router()
+
+    def add_route(self, path, endpoint, methods=["GET"]):
+        self.router.add_route(path, endpoint, methods)
+
+    async def __call__(self, scope, receive, send):
+        assert scope["type"] == "http"
+
+        path = scope["path"]
+        method = scope["method"]
+
+        handler = self.router.get_route(path, method)
+
+        if handler:
+            response = await handler()
+        else:
+            response = Response("Not Found", status_code=404)
+
+        await response(scope, receive, send)
